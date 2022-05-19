08ad45c7cd7407a6a42b03cfad5f399992b00288
Keith Fletcher <sourkidk@gmail.com>
2022-05-19 05:57:41 -0400

    All FXML drafted. All controllers generated.

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../view/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("../view/MainForm.fxml"));
+        primaryStage.setTitle("Inventory Management System");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
